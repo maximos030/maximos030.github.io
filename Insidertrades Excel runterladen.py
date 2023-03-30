ea3138695f99a60ee7059f5c7e43197058e021b6
maximos030 <119617252+maximos030@users.noreply.github.com>
2023-03-30 23:54:57 +0200

    Delete Insidertrades Excel runterladen.py

--- Insidertrades Excel runterladen.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = "https://portal.mvp.bafin.de/database/DealingsInfo/sucheForm.do?meldepflichtigerName=&zeitraum=2&d-4000784-e=1&emittentButton=Suche+Emittent&emittentName=&zeitraumVon=01.03.2023&emittentIsin=&6578706f7274=1&zeitraumBis=31.03.2023"
-
-response = requests.get(url)
-
-with open('C:/Users/mxms2/OneDrive/Dokumente/Aktien/EXCEL/bafin_dealings.csv', 'wb') as f:
-    f.write(response.content)
