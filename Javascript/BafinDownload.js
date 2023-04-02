ce81432ecb610ea1d2b6ddbb41ce28a4edf6af2e
maximos030 <119617252+maximos030@users.noreply.github.com>
2023-04-02 23:42:27 +0200

    Delete BafinDownload.js

--- Javascript/BafinDownload.js
+++
@@ -1,9 +0,0 @@
-function downloadCSV() {
-    var csvUrl = "https://portal.mvp.bafin.de/database/DealingsInfo/sucheForm.do?meldepflichtigerName=&zeitraum=2&d-4000784-e=1&emittentButton=Suche+Emittent&emittentName=&zeitraumVon=01.03.2023&emittentIsin=&6578706f7274=1&zeitraumBis=31.03.2023";
-    var csvFilename = "bafin_dealings.csv";
-    var downloadLink = document.getElementById("csvDownloadLink");
-    downloadLink.href = csvUrl;
-    downloadLink.download = csvFilename;
-    downloadLink.click();
-}
-
